9b3662fe836713a5bd1fb6847cf3a13761975d13
Ming Ming <mint2648@gmail.com>
2015-03-28 05:24:10 +0800

    Add dumb Log

---
+++ src/main/java/com/nkming/powermenu/Log.java
@@ -0,0 +1,13 @@
+/*
+ * Log.java
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2015 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+package com.nkming.powermenu;
+
+public class Log extends com.nkming.utils.Log
+{
+}
