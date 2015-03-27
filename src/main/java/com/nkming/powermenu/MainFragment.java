323fadcbb44602ff222ba6f93fdb3638acbe9dba
Ming Ming <mint2648@gmail.com>
2015-03-28 03:53:34 +0800

    Add some nice animation for the buttons

--- src/main/java/com/nkming/powermenu/MainFragment.java
+++ src/main/java/com/nkming/powermenu/MainFragment.java
@@ -14,7 +14,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import com.shamanland.fab.FloatingActionButton;
 
@@ -32,11 +32,29 @@ public class MainFragment extends Fragment
 	private void initButton(View root)
 	{
 		mShutdownBtn = (FloatingActionButton)root.findViewById(R.id.shutdown_id);
-		mRestartBtn = (FloatingActionButton)root.findViewById(R.id.restart_btn);
 		mSleepBtn = (FloatingActionButton)root.findViewById(R.id.sleep_btn);
+		mRestartBtn = (FloatingActionButton)root.findViewById(R.id.restart_btn);
+
+		mShutdownBtn.setScaleX(0.0f);
+		mShutdownBtn.setScaleY(0.0f);
+		mShutdownBtn.animate().scaleX(1.0f).scaleY(1.0f)
+				.setInterpolator(new DecelerateInterpolator())
+				.setDuration(250);
+
+		mSleepBtn.setScaleX(0.0f);
+		mSleepBtn.setScaleY(0.0f);
+		mSleepBtn.animate().scaleX(1.0f).scaleY(1.0f)
+				.setInterpolator(new DecelerateInterpolator())
+				.setDuration(250).setStartDelay(100);
+
+		mRestartBtn.setScaleX(0.0f);
+		mRestartBtn.setScaleY(0.0f);
+		mRestartBtn.animate().scaleX(1.0f).scaleY(1.0f)
+				.setInterpolator(new DecelerateInterpolator())
+				.setDuration(250).setStartDelay(200);
 	}
 
 	private FloatingActionButton mShutdownBtn;
-	private FloatingActionButton mRestartBtn;
 	private FloatingActionButton mSleepBtn;
+	private FloatingActionButton mRestartBtn;
 }
