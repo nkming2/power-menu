5dd587ed5a86bfe979344760e7efdb2cfde1d259
Ming Ming <mint2648@gmail.com>
2015-04-04 00:33:11 +0800

    Show persistent view on boot

---
+++ src/main/java/com/nkming/powermenu/BootReceiver.java
@@ -0,0 +1,22 @@
+/*
+ * BootReceiver.java
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2015 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+package com.nkming.powermenu;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver
+{
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		PersistentViewHelper.startIfNecessary(context);
+	}
+}
