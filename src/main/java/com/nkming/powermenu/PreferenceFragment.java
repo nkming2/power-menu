d4b02aa7fa0b55259577c8d5836d5fff221b6cfb
Ming Ming <mint2648@gmail.com>
2015-04-09 01:36:44 +0800

    Set the launcher visibility on preference change

--- src/main/java/com/nkming/powermenu/PreferenceFragment.java
+++ src/main/java/com/nkming/powermenu/PreferenceFragment.java
@@ -56,6 +56,11 @@ public class PreferenceFragment extends android.preference.PreferenceFragment
 			PersistentService.setAutohideView(getActivity(),
 					pref.getBoolean(key, false));
 		}
+		else if (key.equals(getString(R.string.pref_hide_launcher_key)))
+		{
+			SystemHelper.setEnableActivity(getActivity(), LauncherActivity.class,
+					!pref.getBoolean(key, false));
+		}
 	}
 
 	private void onPersistentViewChange(SharedPreferences pref, String key)
