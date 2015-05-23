794424dd72b519417eef8cfb1218914fe691e7d1
Ming Ming <mint2648@gmail.com>
2015-05-23 09:01:32 +0800

    Persistent view promoted to lib

--- src/main/java/com/nkming/powermenu/PersistentService.java
+++ src/main/java/com/nkming/powermenu/PersistentService.java
@@ -8,198 +8,27 @@
 
 package com.nkming.powermenu;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
+import android.app.Notification;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
-import android.view.View;
 import android.widget.Toast;
 
 /**
  * Service to display the persistent view
  */
-public class PersistentService extends Service
+public class PersistentService extends com.nkming.utils.widget.PersistentService
 {
-	/**
-	 * Start the service
-	 *
-	 * @param context
-	 */
-	public static void start(Context context)
+	@Override
+	protected int getLayoutId()
 	{
-		Intent intent = new Intent(context, PersistentService.class);
-		context.startService(intent);
-	}
-
-	/**
-	 * Stop the service
-	 *
-	 * @param context
-	 */
-	public static void stop(Context context)
-	{
-		Intent intent = new Intent(context, PersistentService.class);
-		intent.setAction(ACTION_STOP);
-		context.startService(intent);
-	}
-
-	/**
-	 * Show the persistent view managed by this service. The view will be shown
-	 * automatically during the start of this service so you only need to call
-	 * this after hideView()
-	 *
-	 * @param context
-	 * @see PersistentService#hideView(android.content.Context)
-	 */
-	public static void showView(Context context)
-	{
-		Intent intent = new Intent(context, PersistentService.class);
-		intent.setAction(ACTION_SHOW);
-		context.startService(intent);
-	}
-
-	/**
-	 * Temporarily hide the persistent view
-	 *
-	 * @param context
-	 * @see PersistentService#showView(android.content.Context)
-	 */
-	public static void hideView(Context context)
-	{
-		Intent intent = new Intent(context, PersistentService.class);
-		intent.setAction(ACTION_HIDE);
-		context.startService(intent);
-	}
-
-	/**
-	 * To hide the persistent view when running a fullscreen app
-	 *
-	 * @param flag
-	 */
-	public static void setAutohideView(Context context, boolean flag)
-	{
-		Intent intent = new Intent(context, PersistentService.class);
-		intent.setAction(ACTION_AUTOHIDE);
-		intent.putExtra(EXTRA_AUTOHIDE, flag);
-		context.startService(intent);
-	}
-
-	/**
-	 * Return if the service is running or not
-	 *
-	 * @return
-	 */
-	public static boolean isRunning()
-	{
-		return mIsRunning;
+		return R.layout.persistent_view;
 	}
 
 	@Override
-	public IBinder onBind(Intent intent)
-	{
-		return null;
-	}
-
-	@Override
-	public void onCreate()
-	{
-		Log.d(LOG_TAG, "onCreate");
-		mIsRunning = true;
-		super.onCreate();
-		initView();
-		initForeground();
-	}
-
-	@Override
-	public void onDestroy()
-	{
-		Log.d(LOG_TAG, "onDestroy");
-		mIsRunning = false;
-		super.onDestroy();
-		uninitForeground();
-		uninitView();
-	}
-
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId)
-	{
-		if (intent != null && intent.getAction() != null)
-		{
-			switch (intent.getAction())
-			{
-			case ACTION_STOP:
-				stop();
-				break;
-
-			case ACTION_SHOW:
-				show();
-				break;
-
-			case ACTION_HIDE:
-				hide();
-				break;
-
-			case ACTION_AUTOHIDE:
-				mView.setAutohide(intent.getBooleanExtra(EXTRA_AUTOHIDE, false));
-				break;
-			}
-		}
-		return START_STICKY;
-	}
-
-	private static final String LOG_TAG =
-			PersistentService.class.getCanonicalName();
-	private static final String ACTION_STOP = "stop";
-	private static final String ACTION_SHOW = "show";
-	private static final String ACTION_HIDE = "hide";
-	private static final String ACTION_AUTOHIDE = "autohide";
-	private static final String EXTRA_AUTOHIDE = "autohide";
-
-	private void initView()
-	{
-		if (mView != null)
-		{
-			uninitView();
-		}
-
-		PersistentView.Config conf = new PersistentView.Config();
-		conf.handler = new Handler();
-		conf.context = this;
-		conf.resId = R.layout.persistent_view;
-		conf.alpha = 0.5f;
-		conf.hiddenW = 0.35f;
-		mView = new PersistentView(conf);
-		mView.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				onViewClick();
-			}
-		});
-		mView.setOnLongClickListener(new View.OnLongClickListener()
-		{
-			@Override
-			public boolean onLongClick(View v)
-			{
-				onViewLongClick();
-				return true;
-			}
-		});
-
-		SharedPreferences pref = getSharedPreferences(getString(
-				R.string.pref_file), Context.MODE_PRIVATE);
-		mView.setAutohide(pref.getBoolean(getString(
-				R.string.pref_autohide_persistent_view_key), false));
-	}
-
-	private void initForeground()
+	protected Notification getForegroundNotification()
 	{
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 		builder.setContentTitle(getString(R.string.notification_title))
@@ -218,26 +47,23 @@ public class PersistentService extends Service
 				PendingIntent.FLAG_UPDATE_CURRENT);
 		builder.setContentIntent(pi);
 
-		startForeground(1, builder.build());
+		return builder.build();
 	}
 
-	private void uninitView()
+	@Override
+	protected void onInitView()
 	{
-		if (mView != null)
-		{
-			mView.destroy();
-			mView = null;
-		}
+		super.onInitView();
+		SharedPreferences pref = getSharedPreferences(getString(
+				R.string.pref_file), Context.MODE_PRIVATE);
+		getView().setAutohide(pref.getBoolean(getString(
+				R.string.pref_autohide_persistent_view_key), false));
 	}
 
-	private void uninitForeground()
+	@Override
+	protected void onViewClick()
 	{
-		stopForeground(true);
-	}
-
-	private void onViewClick()
-	{
-		Log.d(LOG_TAG, "onViewClick");
+		super.onViewClick();
 		SystemHelper.sleep(getApplicationContext(),
 				new SystemHelper.SleepResultListener()
 		{
@@ -253,9 +79,10 @@ public class PersistentService extends Service
 		});
 	}
 
-	private void onViewLongClick()
+	@Override
+	protected void onViewLongClick()
 	{
-		Log.d(LOG_TAG, "onViewLongClick");
+		super.onViewLongClick();
 		SystemHelper.startActivity(MainActivity.class,
 				new SystemHelper.StartActivityResultListener()
 		{
@@ -271,30 +98,4 @@ public class PersistentService extends Service
 			}
 		});
 	}
-
-	private void stop()
-	{
-		mView.hide(new AnimatorListenerAdapter()
-		{
-			@Override
-			public void onAnimationEnd(Animator animation)
-			{
-				stopSelf();
-			}
-		});
-	}
-
-	private void show()
-	{
-		mView.show(null);
-	}
-
-	private void hide()
-	{
-		mView.hide(null);
-	}
-
-	private PersistentView mView;
-
-	private static boolean mIsRunning = false;
 }
