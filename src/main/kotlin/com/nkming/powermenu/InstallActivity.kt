fba029083d09e70b7cc3d315e72110125ba7230b
Ming Ming <mint2648@gmail.com>
2016-04-21 02:55:27 +0800

    Rewrite in kotlin

---
+++ src/main/kotlin/com/nkming/powermenu/InstallActivity.kt
@@ -0,0 +1,44 @@
+package com.nkming.powermenu
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class InstallActivity : AppCompatActivity(), InstallConfirmFragment.Listener
+{
+	override fun onInstallConfirmed()
+	{
+		val f = InstallFragment.create()
+		f.show(supportFragmentManager, "install")
+	}
+
+	protected override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		if (intent.dataString == getString(R.string.data_uninstall))
+		{
+			_onCreateUninstall(savedInstanceState)
+		}
+		else
+		{
+			_onCreateInstall(savedInstanceState)
+		}
+	}
+
+	private fun _onCreateInstall(savedInstanceState: Bundle?)
+	{
+		if (savedInstanceState == null)
+		{
+			val f = InstallConfirmFragment.create()
+			f.show(supportFragmentManager, "install_confirm")
+		}
+	}
+
+	private fun _onCreateUninstall(savedInstanceState: Bundle?)
+	{
+		if (savedInstanceState == null)
+		{
+			val f = UninstallFragment.create()
+			f.show(supportFragmentManager, "uninstall")
+		}
+	}
+}
