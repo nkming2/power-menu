898bddd6c6243aa6e26df73fb5c6d92bf6240544
Ming Ming <mint2648@gmail.com>
2016-04-21 03:01:11 +0800

    Rewrite in kotlin

---
+++ src/main/kotlin/com/nkming/powermenu/LauncherActivity.kt
@@ -0,0 +1,18 @@
+package com.nkming.powermenu
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * This class is a proxy class such that the launcher icon could be disabled
+ */
+class LauncherActivity : AppCompatActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		startActivity(Intent(this, MainActivity::class.java))
+		finish()
+	}
+}
