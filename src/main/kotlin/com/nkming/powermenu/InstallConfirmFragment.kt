68b662bf28727a439b5d7f1ca95a4a4b13ec5841
Ming Ming <mint2648@gmail.com>
2016-04-21 01:07:00 +0800

    Kotlin migration

---
+++ src/main/kotlin/com/nkming/powermenu/InstallConfirmFragment.kt
@@ -0,0 +1,76 @@
+package com.nkming.powermenu
+
+import android.app.Activity
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import com.afollestad.materialdialogs.MaterialDialog
+
+class InstallConfirmFragment : DialogFragment()
+{
+	interface Listener
+	{
+		fun onInstallConfirmed()
+	}
+
+	companion object
+	{
+		@JvmStatic
+		fun create(): InstallConfirmFragment
+		{
+			return InstallConfirmFragment()
+		}
+
+		private val LOG_TAG = InstallConfirmFragment::class.java.canonicalName
+	}
+
+	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog
+	{
+		return MaterialDialog.Builder(activity)
+				.title(R.string.install_confirm_title)
+				.content(R.string.install_confirm_content)
+				.positiveText(R.string.install_confirm_positive)
+				.onPositive(
+				{
+					materialDialog, dialogAction -> (
+					{
+						if (!isDetached)
+						{
+							_listener.onInstallConfirmed()
+						}
+						_isNoFinish = true
+					}())
+				})
+				.negativeText(android.R.string.cancel)
+				.build()
+	}
+
+	override fun onAttach(activity: Activity?)
+	{
+		super.onAttach(activity)
+		if (activity !is Listener)
+		{
+			Log.wtf("$LOG_TAG.onActivityCreated",
+					"Activity must implement Listener")
+			// In case wtf doesn't throw
+			throw IllegalStateException()
+		}
+		else
+		{
+			_listener = activity
+		}
+	}
+
+	override fun onDismiss(dialog: DialogInterface?)
+	{
+		super.onDismiss(dialog)
+		if (activity != null && !_isNoFinish)
+		{
+			activity.finish()
+		}
+	}
+
+	private lateinit var _listener: Listener
+	private var _isNoFinish = false
+}
