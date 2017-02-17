8e58e5f1a23385261704fc14bfc58b2ea0db1b28
Ming Ming <mint2648@gmail.com>
2017-02-17 14:37:01 +0800

    Add some logging on tiles

--- src/main/kotlin/com/nkming/powermenu/QsTileService.kt
+++ src/main/kotlin/com/nkming/powermenu/QsTileService.kt
@@ -15,13 +15,18 @@ open class _BaseTileService : TileService()
 		qsTile.state = Tile.STATE_INACTIVE
 		qsTile.updateTile()
 	}
-
 }
 
 class ShutdownService : _BaseTileService()
 {
+	companion object
+	{
+		private val LOG_TAG = ShutdownService::class.java.canonicalName
+	}
+
 	override fun onClick()
 	{
+		Log.d(LOG_TAG, "onClick()")
 		// We can't show dialog from service
 		val i = Intent(this, ShutdownActivity::class.java)
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
@@ -32,8 +37,14 @@ class ShutdownService : _BaseTileService()
 
 class RebootService : _BaseTileService()
 {
+	companion object
+	{
+		private val LOG_TAG = RebootService::class.java.canonicalName
+	}
+
 	override fun onClick()
 	{
+		Log.d(LOG_TAG, "onClick()")
 		// We can't show dialog from service
 		val i = Intent(this, RebootActivity::class.java)
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
@@ -44,8 +55,14 @@ class RebootService : _BaseTileService()
 
 class RebootRecoveryService : _BaseTileService()
 {
+	companion object
+	{
+		private val LOG_TAG = RebootRecoveryService::class.java.canonicalName
+	}
+
 	override fun onClick()
 	{
+		Log.d(LOG_TAG, "onClick()")
 		// We can't show dialog from service
 		val i = Intent(this, RebootRecoveryActivity::class.java)
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
@@ -56,8 +73,14 @@ class RebootRecoveryService : _BaseTileService()
 
 class RebootBootloaderService : _BaseTileService()
 {
+	companion object
+	{
+		private val LOG_TAG = RebootBootloaderService::class.java.canonicalName
+	}
+
 	override fun onClick()
 	{
+		Log.d(LOG_TAG, "onClick()")
 		// We can't show dialog from service
 		val i = Intent(this, RebootBootloaderActivity::class.java)
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
@@ -68,8 +91,14 @@ class RebootBootloaderService : _BaseTileService()
 
 class SoftRebootService : _BaseTileService()
 {
+	companion object
+	{
+		private val LOG_TAG = SoftRebootService::class.java.canonicalName
+	}
+
 	override fun onClick()
 	{
+		Log.d(LOG_TAG, "onClick()")
 		// We can't show dialog from service
 		val i = Intent(this, SoftRebootActivity::class.java)
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
@@ -80,16 +109,28 @@ class SoftRebootService : _BaseTileService()
 
 class SleepTileService : _BaseTileService()
 {
+	companion object
+	{
+		private val LOG_TAG = SleepTileService::class.java.canonicalName
+	}
+
 	override fun onClick()
 	{
+		Log.d(LOG_TAG, "onClick()")
 		SleepAction(applicationContext)()
 	}
 }
 
 class ScreenshotTileService : _BaseTileService()
 {
+	companion object
+	{
+		private val LOG_TAG = ScreenshotTileService::class.java.canonicalName
+	}
+
 	override fun onClick()
 	{
+		Log.d(LOG_TAG, "onClick()")
 		val i = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
 		sendBroadcast(i)
 
